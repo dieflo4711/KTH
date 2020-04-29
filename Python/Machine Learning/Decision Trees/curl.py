a66cd76cd528278392b3706d4777275905ba04e0
Diego Leon <diegoflores4711@hotmail.com>
2020-04-29 21:24:37 +0200

    Delete curl.py

--- Python/Machine Learning/Decision Trees/curl.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-data = '{"state": "success", "description": "desc", "context": "test2", "target_url": "http://dleon.johvh.se"}'
-
-response = requests.post('https://api.github.com/repos/dieflo4711/test/statuses/1b1ee2e9624e3ebbb636ee536d7a238c0fa5e21c', data=data, auth=('dieflo4711', 'Diego1639!'))
